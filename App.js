c28d16bc5b9761e8edabe9e2684cfbdf53c5e98d
Marc <marc@unit37.org>
2022-09-12 14:47:02 +0300

    init and complete project

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Alert, StyleSheet, Text, View, Button, TextInput, FlatList, StatusBar, Image } from 'react-native';
 
 export default function App() {
+  const [keyword, setKeyword] = useState('');
+  const [recipes, setRecipes] = useState([]);
+ 
+  const getRecipes = () => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${keyword}`)
+    .then(response => response.json())
+    .then(responseJson => setRecipes(responseJson.meals))
+    .catch(error => { 
+        Alert.alert('Error', error); 
+    });    
+  }
+  
+  const listSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "80%",
+          backgroundColor: "gray",
+          marginLeft: "10%"
+        }}
+      />
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar hidden={true} />
+      <FlatList 
+        style={{marginLeft : "10%"}}
+        keyExtractor={(item, index) => index.toString()} 
+        renderItem={({item}) => 
+          <View>
+            <Text style={{fontSize: 18, fontWeight: "bold"}}>{item.strMeal}</Text>
+            <Image 
+              style={styles.thumbnail}
+              source={{uri: item.strMealThumb}} 
+            />
+          </View>}
+        data={recipes} 
+        ItemSeparatorComponent={listSeparator} /> 
+      <TextInput style={{fontSize: 18, width: 200}} placeholder='keyword' 
+        onChangeText={text => setKeyword(text)} />
+      <Button title="Find" onPress={getRecipes
+  } />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ container: {
+  flex: 1,
+  alignItems: 'center',
+  justifyContent: 'center',
+ },
+ thumbnail : {
+  width: 50,
+  height: 50,
+ }
 });
